8604a94280045152fe84bd974d1f79858fd0f6ec
elena <nenprio@gmail.com>
2018-07-25 10:36:44 +0200

    Changed analysis to most modern KD PR for streaming

--- MirrorAnalysis/EventCategorizer.cpp
+++ MirrorAnalysis/EventCategorizer.cpp
@@ -1,5 +1,5 @@
 /**
- *  @copyright Copyright 2017 The J-PET Framework Authors. All rights reserved.
+ *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
@@ -15,7 +15,11 @@
 
 #include <iostream>
 #include <JPetWriter/JPetWriter.h>
+#include <JPetOptionsTools/JPetOptionsTools.h>
 #include "EventCategorizer.h"
+#include "../LargeBarrelAnalysis/EventCategorizerTools.h"
+
+using namespace jpet_options_tools;
 
 using namespace std;
 
@@ -25,184 +29,141 @@ bool EventCategorizer::init()
 {
 
   INFO("Event categorization started.");
-  INFO("Looking at two hit Events on Layer 1&2 only - creating only control histograms");
-
+  
   fOutputEvents = new JPetTimeWindow("JPetEvent");
-
-  if (fSaveControlHistos) {
-    getStatistics().createHistogram(
-      new TH1F("two_hit_event_theta_diff",
-               "Abs Theta Difference Between Two Hits in Event",
-               360, -0.5, 359.5)
-    );
-    getStatistics().createHistogram(
-      new TH1F("two_hit_event_theta_diff_cut",
-               "Abs Theta Difference Between Opposite Hits in Event",
-               360, -0.5, 359.5)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_x_pos",
-               "Hits X position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_y_pos",
-               "Hits Y position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_z_pos",
-               "Hits Z position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_x_pos_cut",
-               "Opposite Hits X position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_y_pos_cut",
-               "Opposite Hits Y position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH1F("hits_z_pos_cut",
-               "Opposite Hits Z position",
-               100, -60.0, 60.0)
-    );
-    getStatistics().createHistogram(
-      new TH2F("hit_distanece_vs_time_diff",
-               "Two Hit distance vs. abs time difference",
-               100, 0.0, 150.0,
-               100, 0.0, 6000.0)
-    );
-    getStatistics().createHistogram(
-      new TH2F("hit_distanece_vs_theta_diff",
-               "Two Hit distance vs. abs theta difference",
-               100, 0.0, 150.0,
-               360, -0.5, 359.5)
-    );
-    getStatistics().createHistogram(
-      new TH2F("hit_distanece_vs_time_diff_cut",
-               "Opposite Hits distance vs. abs time difference",
-               100, 0.0, 150.0,
-               100, 0.0, 6000.0)
-    );
-    getStatistics().createHistogram(
-      new TH2F("hit_distanece_vs_theta_diff_cut",
-               "Opposite Hit distance vs. abs theta difference",
-               100, 0.0, 150.0,
-               360, -0.5, 359.5)
-    );
-
-    getStatistics().createHistogram(
-      new TH2F("3_hit_angles",
-               "3 Hit angles difference 1-2, 2-3",
-               360, -0.5, 359.5,
-               360, -0.5, 359.5)
-    );
+  
+  if ( isOptionSet(fParams.getOptions(), kMinAnnihilationParamKey) )
+	fMinAnnihilationTOT = getOptionAsFloat(fParams.getOptions(), kMinAnnihilationParamKey);
+  if ( isOptionSet(fParams.getOptions(), kMaxAnnihilationParamKey) )
+	fMaxAnnihilationTOT = getOptionAsFloat(fParams.getOptions(), kMaxAnnihilationParamKey);
+  if ( isOptionSet(fParams.getOptions(), kMinDeexcitationParamKey) )
+	fMinDeexcitationTOT = getOptionAsFloat(fParams.getOptions(), kMinDeexcitationParamKey);
+  if ( isOptionSet(fParams.getOptions(), kMaxDeexcitationParamKey) )
+	fMaxDeexcitationTOT = getOptionAsFloat(fParams.getOptions(), kMaxDeexcitationParamKey);
+	
+  if ( isOptionSet(fParams.getOptions(), kMaxZPosParamKey) )
+	fMaxZPos = getOptionAsFloat(fParams.getOptions(), kMaxZPosParamKey);
+  if ( isOptionSet(fParams.getOptions(), kMaxDistOfDecayPlaneFromCenterParamKey) )
+	fMaxDistOfDecayPlaneFromCenter = getOptionAsFloat(fParams.getOptions(), kMaxDistOfDecayPlaneFromCenterParamKey);
+  if ( isOptionSet(fParams.getOptions(), kMaxTimeDiffParamKey) )
+	fMaxTimeDiff = getOptionAsFloat(fParams.getOptions(), kMaxTimeDiffParamKey);
+  if ( isOptionSet(fParams.getOptions(), kBackToBackAngleWindowParamKey) )
+	fBackToBackAngleWindow = getOptionAsFloat(fParams.getOptions(), kBackToBackAngleWindowParamKey);
+  if ( isOptionSet(fParams.getOptions(), kDecayInto3MinAngleParamKey) )
+	fDecayInto3MinAngle = getOptionAsFloat(fParams.getOptions(), kDecayInto3MinAngleParamKey);
+  
+  if (fSaveControlHistos) 
+  {
+	getStatistics().createHistogram(
+	  new TH1F("TOT", "TOT of Hits",
+	    1000, -0.5, 99.5));
+	getStatistics().getHisto1D("TOT")->SetXTitle("TOT [ns]");
+	getStatistics().getHisto1D("TOT")->SetYTitle("Counts");
+		
+	getStatistics().createHistogram(
+	  new TH1F("Positronium_Lifetime", "Annihilation-Deexcitation Hits Time Difference",
+	    4000, -195.5, 195.5));
+	getStatistics().getHisto1D("Positronium_Lifetime")->SetXTitle("Time difference between annihilation and deexcitation hits [ns]");
+	getStatistics().getHisto1D("Positronium_Lifetime")->SetYTitle("Counts");
+    
+	getStatistics().createHistogram(
+	  new TH2F("DecayInto2_XY", "Pos reco",
+	    218, -54.5, 54.5,
+	    218, -54.5, 54.5));
+	getStatistics().getHisto2D("DecayInto2_XY")->SetXTitle("Y [cm]");
+	getStatistics().getHisto2D("DecayInto2_XY")->SetYTitle("X [cm]");
+	
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto2_Z", "Z pos reco",
+	    220, -54.5, 54.5));
+	getStatistics().getHisto1D("DecayInto2_Z")->SetXTitle("Z [cm]");
+	getStatistics().getHisto1D("DecayInto2_Z")->SetYTitle("Counts");
+		
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto2_Angles", "Annihilation Hits angles",
+	    360, -0.5, 359.5));
+	getStatistics().getHisto1D("DecayInto2_Angles")->SetXTitle("Angle between annihilation hits [deg]");
+	getStatistics().getHisto1D("DecayInto2_Angles")->SetYTitle("Counts");	
+		
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto2_TimeDiff", "Annihilation Hits Time Difference",
+	    2000, -0.5, 195.5));
+	getStatistics().getHisto1D("DecayInto2_TimeDiff")->SetXTitle("Time difference between annihilation hits [ns]");
+	getStatistics().getHisto1D("DecayInto2_TimeDiff")->SetYTitle("Counts");	
+	
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto2_Distance", "Annihilation Hit plane distance",
+	    500, -0.5, 49.5));
+	getStatistics().getHisto1D("DecayInto2_Distance")->SetXTitle("Distance of hit surface from zero [cm]");
+	getStatistics().getHisto1D("DecayInto2_Distance")->SetYTitle("Counts");	
+		
+	getStatistics().createHistogram(
+	  new TH2F("DecayInto3_Angles", "Annihilation Hits angles",
+	    360, -0.5, 359.5,
+	    360, -0.5, 359.5));
+	getStatistics().getHisto2D("DecayInto3_Angles")->SetXTitle("Sum of the two smallest angles [deg]");
+	getStatistics().getHisto2D("DecayInto3_Angles")->SetYTitle("Difference of the two smallest angles [deg]");	
+		
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto3_Distance", "3 Annihilation Hit plane distance",
+	    500, -0.5, 49.5));
+	getStatistics().getHisto1D("DecayInto3_Distance")->SetXTitle("Distance of hit surface from zero [cm]");
+	getStatistics().getHisto1D("DecayInto3_Distance")->SetYTitle("Counts");	
+		
+	getStatistics().createHistogram(
+	  new TH1F("DecayInto3_TimeDiff", "Annihilation Hits Time Difference",
+	    2000, -0.5, 195.5));
+	getStatistics().getHisto1D("DecayInto3_TimeDiff")->SetXTitle("Time difference between annihilation hits [ns]");
+	getStatistics().getHisto1D("DecayInto3_TimeDiff")->SetYTitle("Counts");	
+	
+	getStatistics().createHistogram(
+	  new TH1F("Annihilation_Hits_in_event", "Number of Annihilation Hits in Event", 
+	    50, -0.5, 49.5));
+	getStatistics().getHisto1D("Annihilation_Hits_in_event")->SetXTitle("Number of Annihilation Hits in Event");
+	getStatistics().getHisto1D("Annihilation_Hits_in_event")->SetYTitle("Counts");
+		
+	getStatistics().createHistogram(
+	  new TH1F("Deexcitation_Hits_in_event", "Number of Deexcitation Hits in Event", 
+	    50, -0.5, 49.5));
+	getStatistics().getHisto1D("Deexcitation_Hits_in_event")->SetXTitle("Number of Deexcitation Hits in Event");
+	getStatistics().getHisto1D("Deexcitation_Hits_in_event")->SetYTitle("Counts");
+		
   }
   return true;
 }
 
 bool EventCategorizer::exec()
 {
-
-  //Analysis of Events consisting of two hits that come from Layer 1 or 2
-  //Layer 3 is ignored, since it is not callibrated
-  if (auto timeWindow = dynamic_cast<const JPetTimeWindow* const>(fEvent)) {
+  vector<JPetEvent> events;
+  if (auto timeWindow = dynamic_cast<const JPetTimeWindow* const>(fEvent)) 
+  {
     uint n = timeWindow->getNumberOfEvents();
     for (uint i = 0; i < n; ++i) {
 
-      const auto& event = dynamic_cast<const JPetEvent&>(timeWindow->operator[](i));
-
-      if (event.getHits().size() > 1) {
-
-        vector<JPetHit> hits = event.getHits();
-        for (size_t i = 0; i < hits.size(); i++) {
-          for (size_t j = i + 1; j < hits.size(); j++) {
-            JPetHit firstHit = hits.at(i);
-            JPetHit secondHit = hits.at(j);
-
-            if (firstHit.getBarrelSlot().getLayer().getID() != 3
-                && secondHit.getBarrelSlot().getLayer().getID() != 3) {
-
-              float thetaDiff = fabs(firstHit.getBarrelSlot().getTheta()
-                                     - secondHit.getBarrelSlot().getTheta());
-              float timeDiff = fabs(firstHit.getTime() - secondHit.getTime());
-              float distance = sqrt(pow(firstHit.getPosX() - secondHit.getPosX(), 2)
-                                    + pow(firstHit.getPosY() - secondHit.getPosY(), 2)
-                                    + pow(firstHit.getPosZ() - secondHit.getPosZ(), 2));
-
-              if (fSaveControlHistos) {
-                getStatistics().getHisto1D("two_hit_event_theta_diff")
-                ->Fill(thetaDiff);
-                getStatistics().getHisto1D("hits_x_pos")
-                ->Fill(firstHit.getPosX());
-                getStatistics().getHisto1D("hits_y_pos")
-                ->Fill(firstHit.getPosY());
-                getStatistics().getHisto1D("hits_z_pos")
-                ->Fill(firstHit.getPosZ());
-                getStatistics().getHisto1D("hits_x_pos")
-                ->Fill(secondHit.getPosX());
-                getStatistics().getHisto1D("hits_y_pos")
-                ->Fill(secondHit.getPosY());
-                getStatistics().getHisto1D("hits_z_pos")
-                ->Fill(secondHit.getPosZ());
-                getStatistics().getHisto2D("hit_distanece_vs_time_diff")
-                ->Fill(distance, timeDiff);
-                getStatistics().getHisto2D("hit_distanece_vs_theta_diff")
-                ->Fill(distance, thetaDiff);
-                if (thetaDiff >= 180.0 && thetaDiff < 181.0) {
-                  getStatistics().getHisto1D("two_hit_event_theta_diff_cut")
-                  ->Fill(thetaDiff);
-                  getStatistics().getHisto1D("hits_x_pos_cut")
-                  ->Fill(firstHit.getPosX());
-                  getStatistics().getHisto1D("hits_y_pos_cut")
-                  ->Fill(firstHit.getPosY());
-                  getStatistics().getHisto1D("hits_z_pos_cut")
-                  ->Fill(firstHit.getPosZ());
-                  getStatistics().getHisto1D("hits_x_pos_cut")
-                  ->Fill(secondHit.getPosX());
-                  getStatistics().getHisto1D("hits_y_pos_cut")
-                  ->Fill(secondHit.getPosY());
-                  getStatistics().getHisto1D("hits_z_pos_cut")
-                  ->Fill(secondHit.getPosZ());
-                  getStatistics().getHisto2D("hit_distanece_vs_time_diff_cut")
-                  ->Fill(distance, timeDiff);
-                  getStatistics().getHisto2D("hit_distanece_vs_theta_diff_cut")
-                  ->Fill(distance, thetaDiff);
-                }
-              }
-            }
-          }
-        }
-      }
-
-      if (event.getHits().size() == 3) {
-        JPetHit firstHit = event.getHits().at(0);
-        JPetHit secondHit = event.getHits().at(1);
-        JPetHit thirdHit = event.getHits().at(2);
-
-        float theta_1_2 = fabs(firstHit.getBarrelSlot().getTheta()
-                               - secondHit.getBarrelSlot().getTheta());
-        float theta_2_3 = fabs(secondHit.getBarrelSlot().getTheta()
-                               - thirdHit.getBarrelSlot().getTheta());
-
-        getStatistics().getHisto2D("3_hit_angles")
-        ->Fill(theta_1_2, theta_2_3);
-      }
+	const auto& event = dynamic_cast<const JPetEvent&>(timeWindow->operator[](i));
+	if (event.getHits().size() >= 1) 
+	{
+		vector<JPetHit> hits = event.getHits();	 
+		JPetEvent physicEvent = physicsAnalysis( hits );
+		if( physicEvent.getHits().size() )
+			events.push_back( physicEvent );
+	}
+	
+      
     }
-  } else {
-    return false;
+  } 
+  else 
+  {
+	  return false;
   }
+  if( events.size() )
+	saveEvents(events);
+  events.clear();
   return true;
 }
 
 bool EventCategorizer::terminate()
 {
-
-  INFO("More than one hit Events done. Writing conrtrol histograms.");
   return true;
 }
 
@@ -211,4 +172,58 @@ void EventCategorizer::saveEvents(const vector<JPetEvent>& events)
   for (const auto& event : events) {
     fOutputEvents->add<JPetEvent>(event);
   }
+}
+
+JPetEvent EventCategorizer::physicsAnalysis( vector<JPetHit> hits )
+{
+	JPetEvent physicEvent;
+  
+	JPetEvent annihilationHits;
+	JPetEvent deexcitationHits;
+	
+	for( unsigned i=0; i<hits.size(); i++ )
+	{
+		if( fabs( hits[i].getPosZ() ) < fMaxZPos )
+		{
+			double TOTofHit = EventCategorizerTools::calculateTOT( hits[i] );
+			if( fSaveControlHistos )
+			{
+				getStatistics().getHisto1D("TOT")->Fill( TOTofHit );
+			}
+			if( TOTofHit >= fMinAnnihilationTOT && TOTofHit <= fMaxAnnihilationTOT )
+			{
+				physicEvent.addHit( hits[i] );
+				annihilationHits.addHit( hits[i] );
+			}
+			if( TOTofHit >= fMinDeexcitationTOT && TOTofHit <= fMaxDeexcitationTOT )
+			{
+				physicEvent.addHit( hits[i] );
+				deexcitationHits.addHit( hits[i] );
+			}
+		}
+	}
+	
+	if( fSaveControlHistos )
+	{
+		getStatistics().getHisto1D("Annihilation_Hits_in_event")->Fill( annihilationHits.getHits().size() );
+		getStatistics().getHisto1D("Deexcitation_Hits_in_event")->Fill( deexcitationHits.getHits().size() );
+	}
+	if( deexcitationHits.getHits().size() > 0 )
+	{
+		physicEvent.setEventType(JPetEventType::kPrompt);
+		if( annihilationHits.getHits().size() > 0 )
+		{
+			getStatistics().getHisto1D("Positronium_Lifetime")->Fill( annihilationHits.getHits().at(0).getTime()/1000. - deexcitationHits.getHits().at(0).getTime()/1000. );
+		}
+	}
+	if( EventCategorizerTools::checkFor2Gamma(annihilationHits, getStatistics(), fSaveControlHistos, fBackToBackAngleWindow, fMaxTimeDiff, fMaxDistOfDecayPlaneFromCenter) )
+	{
+		physicEvent.setEventType(JPetEventType::k2Gamma);
+	}
+	if( EventCategorizerTools::checkFor3Gamma(annihilationHits, getStatistics(), fSaveControlHistos, fDecayInto3MinAngle, fMaxTimeDiff, fMaxDistOfDecayPlaneFromCenter) )
+	{
+		physicEvent.setEventType(JPetEventType::k3Gamma);
+	}
+	
+	return physicEvent;
 }
--- MirrorAnalysis/EventCategorizer.h
+++ MirrorAnalysis/EventCategorizer.h
@@ -1,5 +1,5 @@
 /**
- *  @copyright Copyright 2017 The J-PET Framework Authors. All rights reserved.
+ *  @copyright Copyright 2018 The J-PET Framework Authors. All rights reserved.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may find a copy of the License in the LICENCE file.
@@ -15,12 +15,13 @@
 
 #ifndef EVENTCATEGORIZER_H
 #define EVENTCATEGORIZER_H
-
 #include <vector>
 #include <map>
-#include <JPetUserTask/JPetUserTask.h>
 #include <JPetHit/JPetHit.h>
 #include <JPetEvent/JPetEvent.h>
+#include <JPetEventType/JPetEventType.h>
+#include <JPetUserTask/JPetUserTask.h>
+#include <JPetStatistics/JPetStatistics.h>
 
 class JPetWriter;
 
@@ -29,15 +30,41 @@ class JPetWriter;
 #endif
 
 class EventCategorizer : public JPetUserTask{
-public:
+public:  
 	EventCategorizer(const char * name);
 	virtual ~EventCategorizer(){}
 	virtual bool init() override;
 	virtual bool exec() override;
 	virtual bool terminate() override;
+	JPetEvent physicsAnalysis( std::vector<JPetHit> hits );
 
 protected:
+	
+	double fMinAnnihilationTOT = 10000.0;
+	double fMaxAnnihilationTOT = 25000.0;
+	const std::string kMinAnnihilationParamKey = "EventCategorizer_MinAnnihilationTOT_float";
+	const std::string kMaxAnnihilationParamKey = "EventCategorizer_MaxAnnihilationTOT_float";
+	
+	double fMinDeexcitationTOT = 30000.0;
+	double fMaxDeexcitationTOT = 50000.0;
+	const std::string kMinDeexcitationParamKey = "EventCategorizer_MinDeexcitationTOT_float";
+	const std::string kMaxDeexcitationParamKey = "EventCategorizer_MaxDeexcitationTOT_float";
+	
+	double fMaxZPos = 23.;
+	double fMaxDistOfDecayPlaneFromCenter = 5.;
+	double fMaxTimeDiff = 1.;
+	double fBackToBackAngleWindow = 3.;
+	double fDecayInto3MinAngle = 190.;
+	const std::string kMaxZPosParamKey = "EventCategorizer_MaxHitZPos_float";
+	const std::string kMaxDistOfDecayPlaneFromCenterParamKey = "EventCategorizer_MaxDistOfDecayPlaneFromCenter_float";
+	const std::string kMaxTimeDiffParamKey = "EventCategorizer_MaxTimeDiff_float";
+	const std::string kBackToBackAngleWindowParamKey = "EventCategorizer_BackToBackAngleWindow_float";
+	const std::string kDecayInto3MinAngleParamKey = "EventCategorizer_DecayInto3MinAngle_float";
+	
 	void saveEvents(const std::vector<JPetEvent>& event);
 	bool fSaveControlHistos = true;
 };
-#endif /*  !EVENTCATEGORIZER_H */
+
+
+#endif
+
